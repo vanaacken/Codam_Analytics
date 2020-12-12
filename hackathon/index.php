3fabefd50cd0d2f66c876b0e3d7225d532393630
Niels van Aacken <niels@Nielss-MBP.fritz.box>
2020-12-12 17:49:59 +0100

    site upload

---
+++ hackathon/index.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Codam Analytics</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css"/>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+</head>
+<body>
+
+
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-center">
+    <a class="uk-navbar-item uk-logo uk-text-center" href="#">CODAM ANALYTICS <br> VENMO AWARENESS CAMPAIGN</a>  
+    </div>
+</nav>
+
+<div class="uk-grid-small uk-grid">
+    <div class="uk-width-1-7">
+    </div>
+    <div class="uk-width-5-7">
+<div class="uk-card uk-card-default uk-card-body">
+<article class="uk-article">
+
+<h1 class="uk-article-title"><a class="uk-link-reset" href="">Your privacy matters.</a></h1>
+
+<p class="uk-article-meta">Written by Codam Analytics on 12 December 2020.</p>
+
+<p class="uk-text-lead">People of the 21st century identify the invasion of privacy as one of the primary concerns of our time. With the rapid rise of our technology, the security issues are more rampant than ever, ranging from web site tracking, sales of personal information databases, telephone records, mail monitoring, and the like. These ever-expanding risks infiltrated themselves into our private lives on a daily basis.</p>
+
+<p>Many companies around the world have access to our digital footprint which can be exploited by anyone who is able to find them. As members of the society, we tend to put the responsibility solely on the companies, who by their nature have access to all our information. Our team’s idea is to focus more on the collaboration between different parties who have control and authority on the decisions we all make so we can gradually create a safer online environment for all.</p>
+
+</article>
+</div>
+    </div>
+</div>
+
+<div class="uk-grid-small" uk-grid>
+
+    <div class="uk-width-1-4@s">
+        <div class="uk-card uk-card-default uk-card-body">
+            <div class="uk-card-header">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title uk-margin-remove-bottom">Fight for private by default!</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-card-body">
+                <p>To learn more about internet safety<br> 
+                <a class="uk-link-muted" href="https://youtu.be/dQw4w9WgXcQ">Click here</a> </p>
+            </div>
+        </div>
+    </div>
+    <div class="uk-width-3-4@s">
+        <div class="uk-card uk-card-default uk-card-body">
+            <div class="uk-card-header">
+                <p><h3>Your public privacy settings affects more than you think...</h3></p>
+            </div>
+            <div class="uk-card-body"> 
+                <div class="uk-form-controls">
+				    <input class="uk-input" id="form-text" type="text" placeholder="Enter your Venmo username to learn the truth">
+				</div>
+            </div>
+            <div class="uk-card-footer">
+                <div class="uk-margin">
+                <a href="result.php"><input class="uk-button uk-button-danger uk-button-large"  type="submit" name="submit" value="Learn the TRUTH"> </a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+</body>
+</html>
