b993ce33a90e0cd969b85f7e2f1c6a179d0ab4f6
Niels van Aacken <niels@Nielss-MBP.fritz.box>
2020-12-12 17:53:35 +0100

    cool changes

---
+++ site/result.php
@@ -0,0 +1,164 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] = "POST")
+{
+	// $connection = new MongoClient(); // connects to localhost:27017
+	// $connection = new MongoClient( "mongodb://example.com" ); // connect to a remote host (default port: 27017)
+	// $connection = new MongoClient( "mongodb://example.com:65432" ); // connect to a remote host at a given port
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Codam Analytics</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/uikit.min.css"/>
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+</head>
+<body>
+
+
+<nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-center">
+    <a class="uk-navbar-item uk-logo uk-text-center" href="index.php">CODAM ANALYTICS <br> VENMO AWARENESS CAMPAIGN</a>  
+    </div>
+</nav>
+
+<div class="uk-grid-small uk-grid">
+    <div class="uk-width-1-7">
+    </div>
+    <div class="uk-width-5-7">
+		<div class="uk-card uk-card-default uk-card-body">
+		<article class="uk-article">
+
+			<h1 class="uk-article-title"><a class="uk-link-reset" href="">Do you recognize some of these people?</a></h1>
+			<p class="uk-text-lead">Based on your public payment data via venmo we are able to find your friends and families information as well.
+				<br>Practice social distancing online and always set your settings to PRIVATE!</p>
+
+
+		</article>
+		</div>
+    </div>
+</div>
+
+
+<div class="uk-grid-small" uk-grid="masonry: true">
+
+    <div class="uk-width-1-4@s">
+        <div class="uk-card uk-card-default uk-card-body">
+            <div class="uk-card-header">
+                <div class="uk-grid-small uk-flex-middle" uk-grid>
+                    <div class="uk-width-expand">
+                        <h3 class="uk-card-title uk-margin-remove-bottom">Fight for private by default!</h3>
+                    </div>
+                </div>
+            </div>
+            <div class="uk-card-body">
+				<p>To learn more about internet safety <br>
+				<a class="uk-link-muted" href="https://youtu.be/dQw4w9WgXcQ">Click here</a></p>
+            </div>
+        </div>
+    </div>
+
+	<div class="uk-width-1-4@s">
+		<div class="uk-card uk-card-default uk-card-body">
+    		<div class="uk-card-header">
+       			 <div class="uk-grid-small uk-flex-middle" uk-grid>
+           			 <div class="uk-width-expand">
+               			 <h3 class="uk-card-title uk-margin-remove-bottom">Drew-Christianson-1</h3>
+            		</div>
+        		</div>
+    		</div>
+    			<div class="uk-card-body">
+					<div class="uk-width-auto">
+	      				 <img class="uk-border-circle" width="150" height="150" src=https://venmopics.appspot.com/u/v1/s/2224efe4-a5ee-47bd-847a-9d2f3a2984fa>
+         			</div>
+    			</div>
+			</div>
+	</div>
+	<div class="uk-width-1-4@s">
+		<div class="uk-card uk-card-default uk-card-body">
+    		<div class="uk-card-header">
+       			 <div class="uk-grid-small uk-flex-middle" uk-grid>
+           			 <div class="uk-width-expand">
+               			 <h3 class="uk-card-title uk-margin-remove-bottom">Catherine-West-16</h3>
+            		</div>
+        		</div>
+    		</div>
+    			<div class="uk-card-body">
+					<div class="uk-width-auto">
+	      				 <img class="uk-border-circle" width="150" height="150" src=https://s3.amazonaws.com/venmo/no-image.gif>
+         			</div>
+    			</div>
+			</div>
+	</div>
+	<div class="uk-width-1-4@s">
+		<div class="uk-card uk-card-default uk-card-body">
+    		<div class="uk-card-header">
+       			 <div class="uk-grid-small uk-flex-middle" uk-grid>
+           			 <div class="uk-width-expand">
+               			 <h3 class="uk-card-title uk-margin-remove-bottom">lostguylane</h3>
+            		</div>
+        		</div>
+    		</div>
+    			<div class="uk-card-body">
+					<div class="uk-width-auto">
+	      				 <img class="uk-border-circle" width="150" height="150" src=https://venmopics.appspot.com/u/v1/s/835de0f0-7d7b-49b7-ba7c-fc5c8a58887a>
+         			</div>
+    			</div>
+			</div>
+	</div>
+	<div class="uk-width-1-4@s">
+		<div class="uk-card uk-card-default uk-card-body">
+    		<div class="uk-card-header">
+       			 <div class="uk-grid-small uk-flex-middle" uk-grid>
+           			 <div class="uk-width-expand">
+               			 <h3 class="uk-card-title uk-margin-remove-bottom">Kmurph7818</h3>
+            		</div>
+        		</div>
+    		</div>
+    			<div class="uk-card-body">
+					<div class="uk-width-auto">
+	      				 <img class="uk-border-circle" width="150" height="150" src=https://venmopics.appspot.com/u/v1/f/8e8e0bb9-30ce-42a3-a62f-893112794703>
+         			</div>
+    			</div>
+			</div>
+	</div>
+	<div class="uk-width-1-4@s">
+		<div class="uk-card uk-card-default uk-card-body">
+    		<div class="uk-card-header">
+       			 <div class="uk-grid-small uk-flex-middle" uk-grid>
+           			 <div class="uk-width-expand">
+               			 <h3 class="uk-card-title uk-margin-remove-bottom">swetha-gurajala</h3>
+            		</div>
+        		</div>
+    		</div>
+    			<div class="uk-card-body">
+					<div class="uk-width-auto">
+	      				 <img class="uk-border-circle" width="150" height="150" src=https://s3.amazonaws.com/venmo/no-image.gif>
+         			</div>
+    			</div>
+			</div>
+	</div>
+	<div class="uk-width-1-4@s">
+		<div class="uk-card uk-card-default uk-card-body">
+    		<div class="uk-card-header">
+       			 <div class="uk-grid-small uk-flex-middle" uk-grid>
+           			 <div class="uk-width-expand">
+               			 <h3 class="uk-card-title uk-margin-remove-bottom">This could be you!</h3>
+            		</div>
+        		</div>
+    		</div>
+    			<div class="uk-card-body">
+					<div class="uk-width-auto">
+	      				 <img class="uk-border-circle" width="150" height="150" src=https://www.liveabout.com/thmb/0DNrjC90zr2BXxMwrhhrDQeOYFs=/2332x1312/smart/filters:no_upscale()/NoMeGusta-5a314fde96f7d00037ef5d65.jpg>
+         			</div>
+    			</div>
+			</div>
+	</div>
+</div>
+
+</body>
+</html>
+
